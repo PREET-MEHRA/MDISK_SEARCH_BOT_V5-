8a8153c956e143af94a0a3ce54226467a725acc0
PREET-MEHRA <119394229+PREET-MEHRA@users.noreply.github.com>
2023-01-02 13:24:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PREET-MEHRA/Mdisk-bot.git /Mdisk-bot
-cd /Mdisk-bot
+git clone https://github.com/PREET-MEHRA/MDISK_SEARCH_BOT_V5-.git /MDISK_SEARCH_BOT_V5-
+cd /MDISK_SEARCH_BOT_V5-
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
